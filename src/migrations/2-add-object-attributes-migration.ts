e96c0535d14c19452df408363ae1daa3bde44fa7
Nicolas Märchy <nm@studer-raimann.ch>
2017-09-04 15:02:01 +0200

    Create migration script to update the objects table

---
+++ src/migrations/2-add-object-attributes-migration.ts
@@ -0,0 +1,41 @@
+
+import {BaseMigration} from "./migration";
+import {SQLiteDatabaseService} from "../services/database.service";
+
+export class AddObjectAttributesMigration
+  extends BaseMigration {
+
+  up(): Promise<any> {
+    return new Promise((resolve, reject) => {
+
+        SQLiteDatabaseService.instance().then(db => {
+
+          db.query(
+            "ALTER TABLE objects" +
+            "ADD hasPageLayout BOOLEAN NOT NULL DEFAULT 0 CHECK (hasPageLayout IN (0,1))"
+          ).then(() => {
+            resolve()
+          }).catch((error) => {
+            reject(error)
+          })
+        })
+    });
+  }
+
+  down(): Promise<any> {
+    return new Promise((resovle, reject) => {
+
+      SQLiteDatabaseService.instance().then(db => {
+
+        db.query("" +
+          "ALTER TABLE objects" +
+          "DROP COLUMN hasPageLayout"
+        ).then(() => {
+          resovle()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    });
+  }
+}
