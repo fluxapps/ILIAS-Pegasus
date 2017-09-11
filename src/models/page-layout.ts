cd2fd887319d6d1e3ff087ee7a457279fcff73be
Nicolas Märchy <nm@studer-raimann.ch>
2017-09-11 08:08:15 +0200

    Add language variables

---
+++ src/models/page-layout.ts
@@ -0,0 +1,7 @@
+/**
+ * Holds the the styling information, if a course has a page layout.
+ */
+export class PageLayout {
+  readonly icon: string = "information-circle";
+  readonly text: string = "object-list.page-layout";
+}
