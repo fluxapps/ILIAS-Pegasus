a5159eade9125486f6ca40be4a24737086e10dab
Nicolas Märchy <nm@studer-raimann.ch>
2017-09-11 10:04:29 +0200

    Add costructor body for the text variable
    The text variable is definded by the passed in type parameter. Depending on the type
    an according language variable is defined.

--- src/models/page-layout.ts
+++ src/models/page-layout.ts
@@ -1,7 +1,28 @@
 /**
- * Holds the the styling information, if a course has a page layout.
+ * Holds the the styling information, if an ILIAS object has a page layout.
  */
 export class PageLayout {
+
   readonly icon: string = "information-circle";
-  readonly text: string = "object-list.page-layout";
+  readonly text: string;
+
+  constructor(
+    type: string = ""
+  ) {
+
+    switch (type) {
+      case "crs":
+        this.text = "page-layout.course";
+        break;
+      case "grp":
+        this.text = "page-layout.group";
+        break;
+      case "fold":
+        this.text = "page-layout.folder";
+        break;
+      default:
+        this.text = "page-layout.default";
+        break;
+    }
+  }
 }
