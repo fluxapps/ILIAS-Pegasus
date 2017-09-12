cec937f28adf3976587f04841dd68d42652dbfdb
Nicolas Märchy <nm@studer-raimann.ch>
2017-09-12 11:26:44 +0200

    Refactor link rewiter class
    To rewrite a link, the ILIAS link object is available. Supported views, that can be
    opened now, are defined in an enum. An interface is created now and the logic to rewrite / convert
    a link is split into different objects.

--- src/services/link-rewriter.service.ts
+++ src/services/link-rewriter.service.ts
@@ -2,12 +2,54 @@ import {ILIASRestProvider} from "../providers/ilias-rest.provider";
 import {User} from "../models/user";
 import {Injectable} from "@angular/core";
 
+/**
+ * Describes a converter for a {@link ILIASLink}.
+ */
+export interface UrlConverter {
+
+  /**
+   * Converts the given {@code link}.
+   *
+   * @param {ILIASLink} link the link to convert
+   * @returns {Promise<string>} the resulting url
+   */
+  convert(link: ILIASLink): Promise<string>
+}
+
+
 @Injectable()
-export class TokenLinkRewriter {
+export class TokenLinkRewriter implements UrlConverter{
 
   public constructor(
     private readonly restProvider: ILIASRestProvider
   ) {}
+
+  /**
+   * Converts the given {@code link} to a specific url, that can be used
+   * by the ILIAS Pegasus Helper Plugin.
+   *
+   * @see https://github.com/studer-raimann/PegasusHelper
+   *
+   * @param {ILIASLink} link the link to convert
+   * @returns {Promise<string>} the resulting url
+   */
+  convert(link: ILIASLink): Promise<string> {
+
+    let userid = 0;
+
+    return User.currentUser()
+      .then(user => {
+        userid = user.iliasUserId;
+        return this.restProvider.getAuthToken(user);
+      })
+      .then(tokenObject => {
+        const url = `${link.host}/goto.php?target=ilias_app_auth|${userid}|${link.refId}|${tokenObject.token}`;
+        return Promise.resolve(url);
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  }
 
   public rewrite(link: string): Promise<string> {
 
@@ -35,4 +77,43 @@ export class TokenLinkRewriter {
         return Promise.reject(error);
       });
   }
+}
+
+/**
+ * Data class containing information for a link to ILIAS.
+ */
+export class ILIASLink {
+
+  readonly host: string;
+  readonly refId: number;
+
+  /**
+   * Extracts the host and the ref id from the given {@code url}.
+   *
+   * @param {string} url a link to ILIAS, must contain 'target'
+   *                  as request parameter with the ref id as value, prefixed ref id is possible
+   *                  e.g. https://ilias.de/goto.php?target=crs_67 or https://ilias.de/goto.php?target=67
+   * @param {string} view the wanted view of the of the object
+   */
+  constructor(
+    url: string,
+    readonly view: ILIASLinkView = ILIASLinkView.DEFAULT
+  ) {
+
+    const pattern: RegExp = new RegExp("(http(?:s?):\\/\\/.*)\\/.*target=[a-z_]*(\\d+)");
+    const matches: string[] = pattern.exec(url);
+
+    // TODO: Check for matches
+    this.host = matches[1];
+    this.refId = parseInt(matches[2], 10);
+  }
+}
+
+/**
+ * All views for an {@link ILIASLinkView}, that are supported
+ * by the {@link TokenLinkRewriter}.
+ */
+export enum ILIASLinkView {
+  DEFAULT,
+  TIMELINE
 }
