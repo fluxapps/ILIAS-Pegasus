3fb0c931aa79a5c31dfaf5b6146659b1ef817ef5
Nicolas Märchy <nm@studer-raimann.ch>
2017-09-12 11:52:01 +0200

    Create Timeline class

---
+++ src/models/timeline.ts
@@ -0,0 +1,32 @@
+/**
+ * Contains the data to display a timeline.
+ */
+export class TimeLine {
+
+  readonly icon: string = "";
+  readonly text: string;
+
+  /**
+   * Sets the text as language variable depending on the given {@code type}.
+   * If the type can not be identified a default text will be set.
+   *
+   *
+   * @param {string} type an ILIAS object type
+   */
+  constructor(
+    type: string
+  ) {
+
+    switch (type) {
+      case "crs":
+        this.text = "timeline.course";
+        break;
+      case "grp":
+        this.text = "timeline.group";
+        break;
+      default:
+        this.text = "timeline.default";
+        break;
+    }
+  }
+}
