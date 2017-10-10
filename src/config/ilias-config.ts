55690c245fd113ff56e0f066728215cabf2668a5
Nicolas Märchy <nm@studer-raimann.ch>
2017-10-10 13:14:22 +0200

    Add config factory to load the configuration file
    The logic to load the configuration file is moved to a factory class. The config class itself is an interface now.

--- src/config/ilias-config.ts
+++ src/config/ilias-config.ts
@@ -1,56 +1,14 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {Log} from "../services/log.service";
 
-@Injectable()
-export class ILIASConfig {
+export interface ILIASConfig {
+  readonly installations: Array<ILIASInstallation>
+}
 
-  protected data: any = {};
-
-  /**
-   * ILIASConfig
-   *
-   * reads to configuration from config.json
-   *
-   * @param http
-   */
-  constructor(private http: Http) {
-    // Log.write(this, "ILIASConfig constructor");
-    this.loadData().subscribe(
-      data => this.data = data
-    );
-  }
-
-
-  loadData() {
-    return this.http.get("assets/config.json").map(data => {
-      return data.json();
-    });
-  }
-
-  /**
-   * Get a config value, returns a Promise resolving the value
-   * @param key
-   */
-  get(key: string): Promise<any> {
-    Log.write(this, "Asking for Config: " + this.data);
-    if (Object.keys(this.data).length) {
-      return Promise.resolve(this.getByKey(key));
-    } else {
-      return Promise.reject("No such config.");
-    }
-  }
-
-  /**
-   * @param key
-   * @returns any
-   */
-  protected getByKey(key: string) {
-    if (!this.data.hasOwnProperty(key)) {
-      return null;
-    }
-
-    return this.data[key];
-  }
-
+export interface ILIASInstallation {
+  readonly id: number;
+  readonly title: string;
+  readonly url: string;
+  readonly clientId: string;
+  readonly apiKey: string;
+  readonly apiSecret: string;
+  readonly accessTokenTTL: number;
 }
