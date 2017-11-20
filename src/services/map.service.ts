4e3782884c4929b8ca424c852c650d9f6d0064b2
Nicolas Märchy <nm@studer-raimann.ch>
2017-11-20 10:18:44 +0100

    Add JSDoc to the map service

--- src/services/map.service.ts
+++ src/services/map.service.ts
@@ -1,23 +1,45 @@
-
 import {
   CameraPosition, GoogleMap, GoogleMapOptions, GoogleMapsEvent, ILatLng, LatLng, MapType,
   MarkerOptions, MyLocation, MyLocationOptions, VisibleRegion
 } from "@ionic-native/google-maps";
 import {isUndefined} from "util";
 
+/**
+ * Describes coordinates by longitude and latitude.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 export interface GeoCoordinate {
   readonly longitude: number,
   readonly latitude: number
 }
 
+/**
+ * Describes a marker that can be placed on a map.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 export interface Marker {
   readonly title: string,
   readonly snippet: string,
   readonly position: GeoCoordinate
 }
 
+/**
+ * Describes a standard map that is used in the map service.
+ * It exposes allowed method from a google map and hides methods
+ * that should not be used directly and only be accessed via
+ * the {@link MapBuilder}.
+ *
+ * @see https://ionicframework.com/docs/native/google-maps
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 export interface StandardMap {
-  
+
   setDiv(domNode: string | HTMLElement): void
 
   getDiv(): HTMLElement
@@ -87,11 +109,21 @@ export interface StandardMap {
   setPadding(top: number, right: number, left: number, bottom: number): void
 }
 
+/**
+ * {@link StandardMapBinding} is a abstraction from the {@link GoogleMap}.
+ * Its used as a wrapper class, because some methods of the google map
+ * should only be accessed via the {@link MapBuilder}.
+ *
+ * Methods are delegated to the given {@code map} instance.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 class StandardMapBinding implements StandardMap {
 
-    constructor(
-      private readonly map: GoogleMap
-    ) {}
+  constructor(
+    private readonly map: GoogleMap
+  ) {}
 
   setDiv(domNode: string | HTMLElement): void { this.map.setDiv(domNode) }
 
@@ -174,12 +206,32 @@ class StandardMapBinding implements StandardMap {
   }
 }
 
+/**
+ * Builder class to create a {@link StandardMap}.
+ *
+ * The builder requires some options to build the map:
+ * - camera - {@link MapBuilder#camera}
+ * - node to bind - {@link MapBuilder#bind}
+ *
+ * In order to use the {@link MapBuilder#bind} properly, this builder
+ * MUST be used in a class, that has access to the given node.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 export class MapBuilder {
 
   private cameraPosition?: CameraPosition<ILatLng>;
   private markerOptions?: MarkerOptions;
   private binding?: string | HTMLElement;
 
+  /**
+   * Uses the given {@code position} as the map camera.
+   *
+   * @param {GeoCoordinate} position the position to set the camera
+   *
+   * @returns {MapBuilder} this instance
+   */
   camera(position: GeoCoordinate): MapBuilder {
 
     this.cameraPosition = <CameraPosition<ILatLng>>{
@@ -194,6 +246,13 @@ export class MapBuilder {
     return this;
   }
 
+  /**
+   * Uses the given {@code marker} on the map.
+   *
+   * @param {Marker} marker the marker to add to the map
+   *
+   * @returns {MapBuilder} this instance
+   */
   marker(marker: Marker): MapBuilder {
 
     this.markerOptions = <MarkerOptions>{
@@ -210,6 +269,13 @@ export class MapBuilder {
     return this;
   }
 
+  /**
+   * Binds the built map to the given {@code node}.
+   *
+   * @param {string | HTMLElement} node the node to bind the map on
+   *
+   * @returns {MapBuilder} this instance
+   */
   bind(node: string | HTMLElement): MapBuilder {
 
     this.binding = node;
@@ -217,6 +283,15 @@ export class MapBuilder {
     return this;
   }
 
+  /**
+   * Builds the map depending on the used options.
+   *
+   * This methods awaits the ready event of the created map.
+   * @see GoogleMap#one, GoogleMapsEvent#MAP_READY
+   *
+   * @returns {Promise<StandardMap>}
+   * @throws {MapEvaluationError} if the builder is not used properly
+   */
   async build(): Promise<StandardMap> {
 
     this.throwIfUndefined(this.cameraPosition, () => {
@@ -227,30 +302,51 @@ export class MapBuilder {
       return new MapEvaluationError("Can not build map: Requires a node to bind the map")
     });
 
-    const map: GoogleMap = new GoogleMap(this.binding, <GoogleMapOptions>{camera: this.cameraPosition});
+    const googleMap: GoogleMap = new GoogleMap(this.binding, <GoogleMapOptions>{camera: this.cameraPosition});
 
-    await map.one(GoogleMapsEvent.MAP_READY);
+    await googleMap.one(GoogleMapsEvent.MAP_READY);
 
     this.applyIfNotUndefined(this.markerOptions, async(options: MarkerOptions) => {
-      await map.addMarker(options);
+      await googleMap.addMarker(options);
     });
 
-    return Promise.resolve(map);
+    return Promise.resolve(new StandardMapBinding(googleMap));
   }
 
+  /**
+   * Throws the given {@code error} if the given {@code object} is undefined.
+   *
+   * @param {T} object the object to check
+   * @param {() => Error} error the error to throw
+   *
+   * @throws Error if the given {@code object} is undefined
+   */
   private throwIfUndefined<T>(object: T | undefined, error: () => Error): void {
     if (isUndefined(object)) {
       throw error();
     }
   }
 
-  private applyIfNotUndefined<T>(object: T | undefined, block: (object: T) => void): void {
+  /**
+   * Executes the given {@code block} if the given {@code object} is not undefined.
+   *
+   * @param {T} object the object to check
+   * @param {(object: T) => void} apply the function to execute with the given {@code object} as parameter
+   */
+  private applyIfNotUndefined<T>(object: T | undefined, apply: (object: T) => void): void {
     if (!isUndefined(object)) {
-      block(object);
+      apply(object);
     }
   }
 }
 
+/**
+ * Specific error, if the {@link MapBuilder} is not used properly.
+ * Indicates a wrong or missing used option.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 export class MapEvaluationError extends Error {
 
   constructor(message: string) {
