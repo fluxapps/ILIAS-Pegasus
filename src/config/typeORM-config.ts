9cc7f6e28380a60f77cde7c560603ea4047e16f8
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-14 16:53:03 +0100

    Add jsdoc to configuration adapter

--- src/config/typeORM-config.ts
+++ src/config/typeORM-config.ts
@@ -1,22 +1,22 @@
-import {File} from "@ionic-native/file";
 import {Injectable} from "@angular/core";
 import {DatabaseConfigurationAdapter, DatabaseConnectionRegistry} from "../services/database/database.api";
 
 export const PEGASUS_CONNECTION_NAME: string = "ilias-pegasus";
 
 /**
- * Configuraten adapter for typeORM connections
+ * Configuration adapter for typeORM connections.
  *
  * @author nmaerchy <nm@studer-raimann.ch>
- * @version 0.0.1
+ * @version 1.0.0
  */
 @Injectable()
 export class TypeORMConfigurationAdapter implements DatabaseConfigurationAdapter {
 
-  constructor(
-    private readonly file: File
-  ) {}
-
+  /**
+   * Adds the {@link PEGASUS_CONNECTION_NAME} to the registry.
+   *
+   * @param {DatabaseConnectionRegistry} registry
+   */
   addConnections(registry: DatabaseConnectionRegistry): void {
     registry.addConnection(PEGASUS_CONNECTION_NAME)
       .setDirectory("assets/");
