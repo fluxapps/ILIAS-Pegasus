6c54d6b6512f4150ba08cc273393caa07262ad43
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-15 11:14:04 +0100

    Implement DBMigration with own migration table

--- src/services/migration/migration.service.ts
+++ src/services/migration/migration.service.ts
@@ -1,13 +1,16 @@
-import {Connection, getConnection} from "typeorm";
+import {Connection, getConnection, QueryRunner} from "typeorm";
 import {Inject, Injectable} from "@angular/core";
 import {PEGASUS_CONNECTION_NAME} from "../../config/typeORM-config";
-import {DBMigration, MIGRATION_SUPPLIER, MigrationSupplier} from "./migration.api";
+import {
+  DBMigration, Migration, MIGRATION_SUPPLIER, MigrationError, MigrationSupplier,
+  MigrationVersion
+} from "./migration.api";
 
 /**
  * DB Migration with TypeORM.
  *
  * @author nmaerchy <nm@studer-raimann.ch>
- * @version 1.0.0
+ * @version 2.0.0
  */
 @Injectable()
 export class TypeOrmDbMigration implements DBMigration {
@@ -26,8 +29,26 @@ export class TypeOrmDbMigration implements DBMigration {
     try {
 
       const connection: Connection = getConnection(PEGASUS_CONNECTION_NAME);
-      await connection.runMigrations();
-      console.log("finish migration");
+
+      const queryRunner: QueryRunner = connection.createQueryRunner();
+
+      const migrationTable: CreateMigrationTable = new CreateMigrationTable();
+      await migrationTable.up(queryRunner);
+
+      const migrations: Array<Migration> = await this.migrationSupplier.get();
+
+      migrations.forEach(async(it) => {
+
+        const result: Array<{}> = await queryRunner.query("SELECT * FROM migrations WHERE id = ?", [it.version.getVersion()]);
+        console.log(result);
+        if (result.length < 1) {
+
+          await it.up(queryRunner);
+
+          await queryRunner.query("INSERT INTO migrations (id) VALUES (?)", [it.version.getVersion()])
+        }
+      })
+
     } catch (error) {
       throw new MigrationError("Could not finish database migration");
     }
@@ -48,8 +69,16 @@ export class TypeOrmDbMigration implements DBMigration {
 
       const connection: Connection = getConnection(PEGASUS_CONNECTION_NAME);
 
+      const queryRunner: QueryRunner = connection.createQueryRunner();
+
+      const migrations: Array<Migration> = await this.migrationSupplier.get();
+
       for(;currentStep < steps; currentStep++) {
-        await connection.undoLastMigration();
+
+        const migration: Migration = await migrations.pop();
+
+        await migration.down(queryRunner);
+        await queryRunner.query("DELETE FROM migrations WHERE id = ?", [migration.version.getVersion()])
       }
 
     } catch (error) {
@@ -59,15 +88,44 @@ export class TypeOrmDbMigration implements DBMigration {
 }
 
 /**
- * Indicates a failure during a database migration.
+ * A simple migration supplier, that supplies migrations,
+ * that are created in this class.
  *
  * @author nmaerchy <nm@studer-raimann.ch>
  * @version 1.0.0
  */
-export class MigrationError extends Error {
+@Injectable()
+export class SimpleMigrationSupplier implements MigrationSupplier {
 
-  constructor(message: string) {
-    super(message);
-    Object.setPrototypeOf(this, MigrationError.prototype);
+  /**
+   * Returns all migration that are being executed by the {@link DBMigration}.
+   * The migrations are executed in the order of the returned array.
+   *
+   * @returns {Promise<Array<Migration>>} the migrations to run
+   */
+  async get(): Promise<Array<Migration>> {
+    return [
+      // Add migrations here
+    ];
+  }
+}
+
+/**
+ * Special migration, which setups the migration table,
+ * to execute further migrations.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
+class CreateMigrationTable implements Migration {
+
+  readonly version: MigrationVersion = new MigrationVersion("V__0");
+
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("CREATE TABLE IF NOT EXISTS migrations (id INTEGER, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)");
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("DELETE TABLE migrations");
   }
 }
