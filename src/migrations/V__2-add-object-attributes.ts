a2096089ae74d1edbd3b5ec80da2db84f9aec3f1
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-15 11:45:19 +0100

    Refactor existing migrations to new migration tool

---
+++ src/migrations/V__2-add-object-attributes.ts
@@ -0,0 +1,49 @@
+/**
+ * Adds additional attributes on the objects table.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
+import {Migration, MigrationVersion} from "../services/migration/migration.api";
+import {QueryRunner} from "typeorm";
+
+export class AddObjectAttributes implements Migration {
+
+  readonly version: MigrationVersion = new MigrationVersion("V__2");
+
+  async up(queryRunner: QueryRunner): Promise<void> {
+
+    await queryRunner.query(
+      "ALTER TABLE objects " +
+      "ADD hasPageLayout BOOLEAN NOT NULL DEFAULT 0 CHECK (hasPageLayout IN (0,1))"
+    );
+
+    await queryRunner.query(
+      "ALTER TABLE objects " +
+      "ADD hasTimeline BOOLEAN NOT NULL DEFAULT 0 CHECK (hasTimeline IN (0,1))"
+    );
+
+    await queryRunner.query(
+      "ALTER TABLE objects " +
+      "ADD permissionType TEXT NOT NULL DEFAULT 'visible'"
+    );
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+
+    await queryRunner.query(
+      "ALTER TABLE objects " +
+      "DROP COLUMN hasPageLayout"
+    );
+
+    await queryRunner.query(
+      "ALTER TABLE objects " +
+      "DROP COLUMN hasTimeline"
+    );
+
+    await queryRunner.query(
+      "ALTER TABLE objects " +
+      "DROP COLUMN permissionType"
+    );
+  }
+}
