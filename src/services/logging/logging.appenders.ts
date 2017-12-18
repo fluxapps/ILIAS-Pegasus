76ae4ea4fc4e78a05daf9be3ea56f5c36bc2bae9
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-18 15:46:07 +0100

    Implement console log appender

--- src/services/logging/logging.appenders.ts
+++ src/services/logging/logging.appenders.ts
@@ -1,14 +1,15 @@
-import {LogEntry, LogLevelAppender} from "./logging.api";
+import {LogEntry, LogLevel, LogLevelAppender} from "./logging.api";
 
 /**
  * An appender that logs to the console.
  *
  * @author nmaerchy <nm@studer-raimann.ch>
- * @version 0.0.1
+ * @version 1.0.0
  */
 export class ConsoleLogAppender extends LogLevelAppender {
 
   protected write(logEntry: LogEntry): void {
-    throw new Error("This method is not implemented yet");
+    const time: Date = new Date(logEntry.time);
+    console.log(`[${time.toLocaleString()}] [${LogLevel[logEntry.level]}] ${logEntry.location} - ${logEntry.message()}`);
   }
 }
