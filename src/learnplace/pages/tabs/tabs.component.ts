c373d23c1fb7af6799d8c5c2669ea9e247785b25
Nicolas Märchy <nm@studer-raimann.ch>
2017-12-22 09:09:53 +0100

    Remove unused code

--- src/learnplace/pages/tabs/tabs.component.ts
+++ src/learnplace/pages/tabs/tabs.component.ts
@@ -1,28 +1,9 @@
 import {Component} from "@angular/core";
 import {MapPage} from "../map/map.component";
-import {LEARNPLACE_REPOSITORY, TypeORMLearnplaceRepository} from "../../providers/repository/learnplace.repository";
-import {MAP_REPOSITORY, TypeORMMapRepository} from "../../providers/repository/map.repository";
-import {LEARNPLACE_LOADER, RestLearnplaceLoader} from "../../services/learnplace";
 
 @Component({
   templateUrl: "tabs.html",
-  providers: [
-    {
-      provide: LEARNPLACE_REPOSITORY,
-      useClass: TypeORMLearnplaceRepository
-    },
-    {
-      provide: MAP_REPOSITORY,
-      useClass: TypeORMMapRepository
-    },
-    {
-      provide: LEARNPLACE_LOADER,
-      useClass: RestLearnplaceLoader
-    }
-  ]
 })
 export class TabsPage {
-
   readonly mapPage: object = MapPage;
-
 }
