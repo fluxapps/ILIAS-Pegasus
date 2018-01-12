ae2c5495b08807f18a6660d92d12cb99f25ceeba
Nicolas Märchy <nm@studer-raimann.ch>
2018-01-12 08:36:20 +0100

    Create wifi access error and roaming access error

--- src/services/device/hardware-features/hardware-access.errors.ts
+++ src/services/device/hardware-features/hardware-access.errors.ts
@@ -25,3 +25,31 @@ export class LocationAccessError extends HardwareAccessError {
     Object.setPrototypeOf(this, LocationAccessError.prototype);
   }
 }
+
+/**
+ * Indicates that the wifi must not be used.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
+export class WifiAccessError extends HardwareAccessError {
+
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, WifiAccessError.prototype);
+  }
+}
+
+/**
+ * Indicates that the roaming must not be used.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
+export class RoamingAccessError extends HardwareAccessError {
+
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, RoamingAccessError.prototype);
+  }
+}
