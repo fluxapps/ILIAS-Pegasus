e5bbb0bf5874e73cbb5225e307f66c458aef19b1
Nicolas Märchy <nm@studer-raimann.ch>
2018-01-12 15:08:38 +0100

    Add jsdoc to diagnostic util

--- src/services/device/hardware-features/diagnostics.util.ts
+++ src/services/device/hardware-features/diagnostics.util.ts
@@ -1,12 +1,33 @@
 import {HardwareAccessError, LocationAccessError, RoamingAccessError, WifiAccessError} from "./hardware-access.errors";
+import {Reject, Resolve} from "../../../declarations";
+import {Logger} from "../../logging/logging.api";
+import {Logging} from "../../logging/logging.service";
 
+const diagnostic: any = window["cordova"].plugins.diagnostic;
+
+/**
+ * Enumerator for all supported hardware features
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
 export enum HardwareFeature {
   LOCATION,
   WIFI,
   ROAMING
 }
 
-export function checkFeature(feature: HardwareFeature): Promise<void> {
+/**
+ * Invokes the appropriate check function of the given feature.
+ *
+ * If the given feature is not supported, the promise will be resolved with the error message.
+ * The reason behind this decision can be reread in the file src/services/device/hardware-features/decorators.ts
+ *
+ * @param {HardwareFeature} feature - the hardware feature to check
+ */
+export function checkFeature(feature: HardwareFeature): Promise<string> {
+
+  const log: Logger = Logging.getLogger("checkFeature");
 
   switch (feature) {
     case HardwareFeature.LOCATION:
@@ -16,59 +37,95 @@ export function checkFeature(feature: HardwareFeature): Promise<void> {
     case HardwareFeature.WIFI:
       return checkWifi();
     default:
-      return Promise.reject(new HardwareAccessError(`Feature could not be evaluated: ${feature}`));
+      log.warn(() => `Could not evaluate hardware feature: ${feature}`);
+      return Promise.resolve(`Could not evaluate feature: ${feature}`);
   }
 }
 
-export function checkLocation(): Promise<void> {
+/**
+ * Checks if the Location can be accessed by this app.
+ *
+ * If the condition can not be evaluated, the promise will be resolved with the error message.
+ * The reason behind this decision can be reread in the file src/services/device/hardware-features/decorators.ts
+ *
+ * @returns {Promise<string>} a message about the location status
+ */
+export function checkLocation(): Promise<string> {
 
-  return new Promise((resolve: () => void, reject: (reason: Error) => void): void => {
+  const log: Logger = Logging.getLogger("checkLocation");
 
-    window["cordova"].plugins.diagnostic.getLocationAuthorizationStatus((status: string): void => {
+  return new Promise((resolve: Resolve<string>, reject: Reject<HardwareAccessError>): void => {
 
-      if (status == window["cordova"].plugins.diagnostic.permissionStatus.DENIED){
+    diagnostic.getLocationAuthorizationStatus((status: string): void => {
+
+      if (status == diagnostic.permissionStatus.DENIED){
         reject(new LocationAccessError("Can not use location: Permission denied"));
       } else {
-        resolve();
+        resolve(`Location Authorization Status: Permission ${status}`);
       }
     }, (msg: string) => {
-      reject(new LocationAccessError(msg));
+      log.warn(() => `Could not evaluate Location Authorization Status: ${msg}`);
+      resolve(msg);
     });
   });
 }
 
-export function checkWifi(): Promise<void> {
+/**
+ * Checks if the wifi can be accessed by this app.
+ *
+ * If the condition can not be evaluated, the promise will be resolved with the error message.
+ * The reason behind this decision can be reread in the file src/services/device/hardware-features/decorators.ts
+ *
+ * @returns {Promise<string>} a message about the wifi status
+ */
+export function checkWifi(): Promise<string> {
 
-  return new Promise((resolve: () => void, reject: (reason: Error) => void): void => {
+  const log: Logger = Logging.getLogger("checkWifi");
 
-    window["cordova"].plugins.diagnostics.isWifiAvailable((available: boolean) => {
+  return new Promise((resolve: Resolve<string>, reject: Reject<HardwareAccessError>): void => {
+
+    diagnostic.isWifiAvailable((available: boolean) => {
 
       if (available) {
-        resolve();
+        resolve("Wifi is available");
       } else {
         reject(new WifiAccessError("Can not use wifi: Not available"));
       }
 
     }, (msg: string) => {
-      reject(new WifiAccessError(msg));
+      log.warn(() => `Could not evaluate Wifi Availability: ${msg}`);
+      resolve(msg);
     });
   });
 }
 
-export function checkRoaming(): Promise<void> {
+/**
+ * ANDROID ONLY!!!
+ *
+ * Checks if the roaming data service can be accessed by this app.
+ *
+ * If the condition can not be evaluated, the promise will be resolved with the error message.
+ * The reason behind this decision can be reread in the file src/services/device/hardware-features/decorators.ts
+ *
+ * @returns {Promise<string>} a message about the roaming data service status
+ */
+export function checkRoaming(): Promise<string> {
 
-  return new Promise((resolve: () => void, reject: (reason: Error) => void): void => {
+  const log: Logger = Logging.getLogger("checkRoaming");
 
-    window["cordova"].plugins.diagnostics.isDataRoamingEnabled((enabled: boolean) => {
+  return new Promise((resolve: Resolve<string>, reject: Reject<HardwareAccessError>): void => {
+
+    diagnostic.isDataRoamingEnabled((enabled: boolean) => {
 
       if (enabled) {
-        resolve();
+        resolve("Data roaming is available");
       } else {
         reject(new RoamingAccessError("Can not use roaming: Not enabled"));
       }
 
     }, (msg: string) => {
-      reject(new RoamingAccessError(msg));
+      log.warn(() => `Could not evaluate roaming data service: ${msg}`);
+      resolve(msg);
     });
   });
 }
