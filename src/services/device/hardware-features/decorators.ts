1f33132ee0aaaac2b967b5d586b60e0071a9c8b0
Nicolas Märchy <nm@studer-raimann.ch>
2018-01-12 09:18:37 +0100

    Create require wifi decorator

--- src/services/device/hardware-features/decorators.ts
+++ src/services/device/hardware-features/decorators.ts
@@ -1,16 +1,35 @@
-import {checkLocation} from "./diagnostics.util";
+import {checkLocation, checkWifi} from "./diagnostics.util";
 
 export function RequireLocation(
   target: object,
   key: string,
   descriptor: TypedPropertyDescriptor<(...args: Array<any>) => Promise<any>>
-): TypedPropertyDescriptor<any> {
+): TypedPropertyDescriptor<(...args: Array<any>) => Promise<any>> {
 
   const method: Function = descriptor.value;
 
-  descriptor.value = function (...args: Array<any>): Promise<any> {
+  descriptor.value = function(...args: Array<any>): Promise<any> {
 
     return checkLocation()
+      .then(function(): Promise<any> {return Promise.resolve(method.apply(this, args))}.bind(this))
+      .catch(err => Promise.reject(err));
+
+  }.bind(this);
+
+  return descriptor;
+}
+
+export function RequireWifi(
+  target: object,
+  key: string,
+  descriptor: TypedPropertyDescriptor<(...args: Array<any>) => Promise<any>>
+): TypedPropertyDescriptor<(...args: Array<any>) => Promise<any>> {
+
+  const method: Function = descriptor.value;
+
+  descriptor.value = function(...args: Array<any>): Promise<any> {
+
+    return checkWifi()
       .then(function(): Promise<any> {return Promise.resolve(method.apply(this, args))}.bind(this))
       .catch(err => Promise.reject(err));
 
