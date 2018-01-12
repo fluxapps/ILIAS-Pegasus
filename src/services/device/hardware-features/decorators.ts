56e2e44cfa9449969bb963132b917aaed4d0eff9
Nicolas Märchy <nm@studer-raimann.ch>
2018-01-12 09:11:50 +0100

    Create require location decorator

--- src/services/device/hardware-features/decorators.ts
+++ src/services/device/hardware-features/decorators.ts
@@ -1,4 +1,20 @@
-export function RequireLocation(target: object, key: string, descriptor: TypedPropertyDescriptor<(...args: Array<any>) => Promise<any>>): TypedPropertyDescriptor<any> {
+import {checkLocation} from "./diagnostics.util";
 
-  throw new Error("This method is not implemented yet");
+export function RequireLocation(
+  target: object,
+  key: string,
+  descriptor: TypedPropertyDescriptor<(...args: Array<any>) => Promise<any>>
+): TypedPropertyDescriptor<any> {
+
+  const method: Function = descriptor.value;
+
+  descriptor.value = function (...args: Array<any>): Promise<any> {
+
+    return checkLocation()
+      .then(function(): Promise<any> {return Promise.resolve(method.apply(this, args))}.bind(this))
+      .catch(err => Promise.reject(err));
+
+  }.bind(this);
+
+  return descriptor;
 }
