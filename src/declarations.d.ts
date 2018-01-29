1884a60516d02d42f0833cf93c43a51d9e511fcf
Jonathan Gerber <jonathangerber@Jonathans-MacBook-Pro-2.local>
2018-01-29 13:24:36 +0100

    small fix q

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -11,24 +11,9 @@
  For more info on type definition files, check out the Typescript docs here:
  https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
  */
-import {IWhenable, Promise} from "q";
 
 // wildcard module
 declare module "*";
-
-// type definitions of q is missing the any function
-declare module "q" {
-
-  /**
-   * Returns a promise that is fulfilled by the first given promise
-   * to be fulfilled, or rejected if all of the given promises are rejected.
-   *
-   * @param {Array<Q.IWhenable<T>>} promises - array of promises to check
-   *
-   * @returns {Promise<T>} the value of the first fulfilled promise
-   */
-  export function any<T>(promises: Array<IWhenable<T>>): Promise<T>;
-}
 
 /**
  * For easier type def in a Promise constructor.
