ca075a5d14b7fc91cdca1d3e045fddb23b297513
Nicolas Märchy <nm@studer-raimann.ch>
2018-02-02 09:03:42 +0100

    Create picture block directive

---
+++ src/learnplace/directives/pictureblock/pictureblock.directive.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from "@angular/core";
+import {PictureBlockModel} from "../../services/block.model";
+
+@Component({
+  selector: "picture-block",
+  templateUrl: "picture-block.html"
+})
+export class PictureBlock {
+
+  @Input("value")
+  readonly picture: PictureBlockModel
+}
