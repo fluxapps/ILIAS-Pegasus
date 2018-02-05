70afd333309d4ce2a50301a84f0fe2d37399fba9
Nicolas Schäfli <ns@studer-raimann.ch>
2018-02-05 17:36:01 +0100

    Add news page content with refresher

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from "@angular/core";
+import {NEWS_FEED, NewsFeed, NewsItemModel} from "../../services/news/news.feed";
+import {Refresher} from "ionic-angular";
+import {NEWS_SYNCHRONIZATION, NewsSynchronization} from "../../services/news/news.synchronization";
 
 /**
  * Generated class for the NewsComponent component.
@@ -7,16 +10,41 @@ import { Component } from '@angular/core';
  * Components.
  */
 @Component({
-  selector: 'news',
-  templateUrl: 'news.html'
+  selector: "news",
+  templateUrl: "news.html"
 })
 export class NewsPage {
 
-  text: string;
+  news: Array<NewsItemModel>;
 
-  constructor() {
-    console.log('Hello NewsComponent Component');
-    this.text = 'Hello World';
+
+  constructor(
+    @Inject(NEWS_FEED) private readonly newsFeed: NewsFeed,
+    @Inject(NEWS_SYNCHRONIZATION) private readonly newsSync: NewsSynchronization
+  ) {
+
+    this.loadNewsFeed();
+  }
+
+  /**
+   * called by pull-to-refresh refresher
+   *
+   * @param {Refresher} refresher
+   * @returns {Promise<void>}
+   */
+  private async startSync(refresher: Refresher): Promise<void> {
+    await this.newsSync.synchronize();
+    await this.loadNewsFeed();
+    refresher.complete();
+  }
+
+  /**
+   * Loads all news of the current user out of the database.
+   *
+   * @returns {Promise<void>}
+   */
+  private async loadNewsFeed(): Promise<void> {
+    this.news = await this.newsFeed.fetchAllForCurrentUser();
   }
 
 }
