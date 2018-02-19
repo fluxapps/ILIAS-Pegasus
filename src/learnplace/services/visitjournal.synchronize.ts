80eab738fd3a30e20dbc4febc700f6ca98673021
Nicolas Märchy <nm@studer-raimann.ch>
2018-02-19 15:24:27 +0100

    Implement visit journal synchronization

--- src/learnplace/services/visitjournal.synchronize.ts
+++ src/learnplace/services/visitjournal.synchronize.ts
@@ -1,6 +1,9 @@
 import {Inject, Injectable, InjectionToken} from "@angular/core";
 import {LEARNPLACE_API, LearnplaceAPI} from "../providers/rest/learnplace.api";
 import {VISIT_JOURNAL_REPOSITORY, VisitJournalRepository} from "../providers/repository/visitjournal.repository";
+import {VisitJournalEntity} from "../entity/visit-journal.entity";
+import {Logger} from "../../services/logging/logging.api";
+import {Logging} from "../../services/logging/logging.service";
 
 /**
  * Describes a synchronization that manages un-synchronized visit journal entries.
@@ -27,6 +30,8 @@ export const VISIT_JOURNAL_SYNCHRONIZATION: InjectionToken<VisitJournalSynchroni
 @Injectable()
 export class VisitJournalSynchronizationImpl implements VisitJournalSynchronization {
 
+  private readonly log: Logger = Logging.getLogger(VisitJournalSynchronizationImpl.name);
+
   constructor(
     @Inject(LEARNPLACE_API) private readonly learnplaceAPI: LearnplaceAPI,
     @Inject(VISIT_JOURNAL_REPOSITORY) private readonly visitJournalRepository: VisitJournalRepository
@@ -41,7 +46,20 @@ export class VisitJournalSynchronizationImpl implements VisitJournalSynchronizat
    * still fails it remains unmodified. If the synchronization is successful
    * its synchronized property will be set to true.
    */
-  synchronize(): Promise<void> {
-    throw new Error("This method is not implemented yet");
+  async synchronize(): Promise<void> {
+
+    const unsynchronized: Array<VisitJournalEntity> = await this.visitJournalRepository.findUnsynchronized();
+
+    for (const it of unsynchronized) {
+      try {
+
+        await this.learnplaceAPI.addJournalEntry(it.learnplace.objectId, it.time);
+        it.synchronized = true;
+        await this.visitJournalRepository.save(it);
+      } catch(error) {
+        this.log.warn(() => `Could not synchronize journal entry: id=${it.id}, username=${it.username}`);
+        this.log.debug(() => `Synchronize Journal Entry Error: ${JSON.stringify(error)}`);
+      }
+    }
   }
 }
