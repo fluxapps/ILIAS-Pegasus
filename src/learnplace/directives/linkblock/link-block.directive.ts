91c6207264f642b4fb364af30d1cbc8ab1066cc9
Nicolas Märchy <nm@studer-raimann.ch>
2018-02-28 13:56:47 +0100

    Add link block label

--- src/learnplace/directives/linkblock/link-block.directive.ts
+++ src/learnplace/directives/linkblock/link-block.directive.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, Input} from "@angular/core";
+import {Component, Inject, Input, OnInit} from "@angular/core";
 import {LinkBlockModel} from "../../services/block.model";
 import {LINK_BUILDER, LinkBuilder} from "../../../services/link/link-builder.service";
 import {
@@ -8,15 +8,22 @@ import {
 import {Builder} from "../../../services/builder.base";
 import {ILIASObjectAction} from "../../../actions/object-action";
 import {TranslateService} from "ng2-translate/src/translate.service";
+import {User} from "../../../models/user";
+import {ILIASObject} from "../../../models/ilias-object";
+import {Logger} from "../../../services/logging/logging.api";
+import {Logging} from "../../../services/logging/logging.service";
 
 @Component({
   selector: "link-block",
   templateUrl: "link-block.html"
 })
-export class LinkBlock {
+export class LinkBlock implements OnInit {
 
   @Input("value")
   readonly link: LinkBlockModel;
+  linkLabel: string | undefined = undefined;
+
+  private readonly log: Logger = Logging.getLogger(LinkBlock.name);
 
   constructor(
     @Inject(LINK_BUILDER) private readonly linkBuilder: LinkBuilder,
@@ -24,6 +31,19 @@ export class LinkBlock {
     @Inject(OPEN_OBJECT_IN_ILIAS_ACTION_FACTORY)
     private readonly openInIliasActionFactory: (title: string, urlBuilder: Builder<Promise<string>>) => OpenObjectInILIASAction,
   ){}
+
+
+  ngOnInit(): void {
+
+    User.findActiveUser().then(user => {
+      return ILIASObject.findByRefId(this.link.refId, user.id);
+    }).then(obj => {
+      this.linkLabel = obj.title;
+    }).catch(_ => {
+      this.log.info(() => `Could not load label for link block with refId: refId=${this.link.refId}`);
+      this.linkLabel = this.translate.instant("learnplace.no-link-label");
+    });
+  }
 
   open(): void {
 
