c769bf552a44a865d569f53882ae6ca9550c0735
Nicolas Märchy <nm@studer-raimann.ch>
2018-03-01 15:03:32 +0100

    Add accordion block mapping

--- src/learnplace/services/block.service.ts
+++ src/learnplace/services/block.service.ts
@@ -1,4 +1,7 @@
-import {BlockModel, LinkBlockModel, PictureBlockModel, TextBlockModel, VideoBlockModel} from "./block.model";
+import {
+  AccordionBlockModel, BlockModel, LinkBlockModel, PictureBlockModel, TextBlockModel,
+  VideoBlockModel
+} from "./block.model";
 import {Inject, Injectable, InjectionToken} from "@angular/core";
 import {LEARNPLACE_REPOSITORY, LearnplaceRepository} from "../providers/repository/learnplace.repository";
 import {VisibilityStrategyApplier} from "./visibility/visibility.context";
@@ -6,6 +9,11 @@ import {LearnplaceEntity} from "../entity/learnplace.entity";
 import {NoSuchElementError} from "../../error/errors";
 import {VisibilityStrategyType} from "./visibility/visibility.strategy";
 import {DomSanitizer} from "@angular/platform-browser";
+import {AccordionEntity} from "../entity/accordion.entity";
+import {TextblockEntity} from "../entity/textblock.entity";
+import {PictureBlockEntity} from "../entity/pictureBlock.entity";
+import {LinkblockEntity} from "../entity/linkblock.entity";
+import {VideoBlockEntity} from "../entity/videoblock.entity";
 
 /**
  * Describes a service that can provide all block types of a single learnplace.
@@ -50,41 +58,58 @@ export class VisibilityManagedBlockService implements BlockService {
     this.strategyApplier.setLearnplace(learnplaceId);
 
     return [
-      ...this.mapTextblocks(learnplace),
-      ...this.mapPictureBlocks(learnplace),
-      ...this.mapLinkBlocks(learnplace),
-      ...this.mapVideoBlocks(learnplace)
+      ...this.mapTextblocks(learnplace.textBlocks),
+      ...this.mapPictureBlocks(learnplace.pictureBlocks),
+      ...this.mapLinkBlocks(learnplace.linkBlocks),
+      ...this.mapVideoBlocks(learnplace.videoBlocks),
+      ...this.mapAccordionBlock(learnplace.accordionBlocks)
     ].sort((a, b) => a.sequence - b.sequence);
   }
 
-  private mapTextblocks(learnplace: LearnplaceEntity): Array<BlockModel> {
-    return learnplace.textBlocks.map(it => {
+  private mapTextblocks(textBlocks: Array<TextblockEntity>): Array<TextBlockModel> {
+    return textBlocks.map(it => {
       const model: TextBlockModel = new TextBlockModel(it.sequence, this.sanitizer.bypassSecurityTrustHtml(it.content));
       this.strategyApplier.apply(model, VisibilityStrategyType[it.visibility.value]);
       return model;
     });
   }
 
-  private mapPictureBlocks(learnplace: LearnplaceEntity): Array<BlockModel> {
-    return learnplace.pictureBlocks.map(it => {
+  private mapPictureBlocks(pictureBlocks: Array<PictureBlockEntity>): Array<PictureBlockModel> {
+    return pictureBlocks.map(it => {
       const model: PictureBlockModel = new PictureBlockModel(it.sequence, it.title, it.description, it.thumbnail, it.url);
       this.strategyApplier.apply(model, VisibilityStrategyType[it.visibility.value]);
       return model;
     });
   }
 
-  private mapLinkBlocks(learnplace: LearnplaceEntity): Array<LinkBlockModel> {
+  private mapLinkBlocks(linkBlocks: Array<LinkblockEntity>): Array<LinkBlockModel> {
 
-    return learnplace.linkBlocks.map(it => {
+    return linkBlocks.map(it => {
       const model: LinkBlockModel = new LinkBlockModel(it.sequence, it.refId);
       this.strategyApplier.apply(model, VisibilityStrategyType[it.visibility.value]);
       return model;
     })
   }
 
-  private mapVideoBlocks(learnplace: LearnplaceEntity): Array<BlockModel> {
-    return learnplace.videoBlocks.map(it => {
+  private mapVideoBlocks(videoBlocks: Array<VideoBlockEntity>): Array<VideoBlockModel> {
+    return videoBlocks.map(it => {
       const model: VideoBlockModel = new VideoBlockModel(it.sequence, it.url);
+      this.strategyApplier.apply(model, VisibilityStrategyType[it.visibility.value]);
+      return model;
+    })
+  }
+
+  private mapAccordionBlock(accordions: Array<AccordionEntity>): Array<AccordionBlockModel> {
+    return accordions.map(it => {
+      const model: AccordionBlockModel = new AccordionBlockModel(
+        it.sequence,
+        [
+          ...this.mapTextblocks(it.textBlocks),
+          ...this.mapPictureBlocks(it.pictureBlocks),
+          ...this.mapLinkBlocks(it.linkBlocks),
+          ...this.mapVideoBlocks(it.videoBlocks)
+        ].sort((a, b) => a.sequence - b.sequence)
+      );
       this.strategyApplier.apply(model, VisibilityStrategyType[it.visibility.value]);
       return model;
     })
