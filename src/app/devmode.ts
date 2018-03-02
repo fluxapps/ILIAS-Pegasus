7c0209764e9cd6dd72a657befb7f70fcfbdd230e
Nicolas Märchy <nm@studer-raimann.ch>
2018-03-02 13:34:57 +0100

    Add jsdoc to function

--- src/app/devmode.ts
+++ src/app/devmode.ts
@@ -1,5 +1,14 @@
 import {isDefined, isFunction} from "ionic-angular/es2015/util/util";
 
+/**
+ * Looks for a property {@code IonicDevServer} on the window.
+ * If the property does exist and the {@code handleError} function is defined,
+ * the ionic dev server is running.
+ *
+ * The common case, when this function returns true, is if the {@code ionic cordova emulate} command is used.
+ *
+ * @returns {boolean} true if the Ionic Dev Server is running, otherwise false
+ */
 export function isDevMode(): boolean {
   const monitor: IonicDevServer = <IonicDevServer>window["IonicDevServer"];
 
