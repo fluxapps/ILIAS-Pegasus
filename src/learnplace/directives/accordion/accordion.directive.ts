27becfd525c89f2772465ec0eca2175df9641245
Nicolas Märchy <nm@studer-raimann.ch>
2018-03-02 11:43:05 +0100

    Implement accordion directive

--- src/learnplace/directives/accordion/accordion.directive.ts
+++ src/learnplace/directives/accordion/accordion.directive.ts
@@ -1,12 +1,31 @@
 import {Component, Input} from "@angular/core";
 import {AccordionBlockModel} from "../../services/block.model";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: "accordion-block",
-  templateUrl: "accordion.html"
+  templateUrl: "accordion.html",
+  animations: [
+    trigger("expanded", [
+      state("0", style({
+        height: "0px",
+        overflow: "hidden"
+      })),
+      state("1", style({
+        height: "*"
+      })),
+      transition("* => *", animate(".5s"))
+    ])
+  ]
 })
 export class AccordionBlock {
 
   @Input("value")
-  readonly accordion: AccordionBlockModel
+  readonly accordion: AccordionBlockModel;
+
+  expanded: boolean = false;
+
+  toggle(): void {
+    this.expanded = !this.expanded;
+  }
 }
