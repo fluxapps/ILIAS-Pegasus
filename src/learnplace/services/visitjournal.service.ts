d274cb87541d3c09661ac658f9b2e00cecddbee7
Nicolas Märchy <nm@studer-raimann.ch>
2018-03-07 10:54:09 +0100

    Create visit journal watch

--- src/learnplace/services/visitjournal.service.ts
+++ src/learnplace/services/visitjournal.service.ts
@@ -4,6 +4,10 @@ import {VISIT_JOURNAL_REPOSITORY, VisitJournalRepository} from "../providers/rep
 import {VisitJournalEntity} from "../entity/visit-journal.entity";
 import {Logger} from "../../services/logging/logging.api";
 import {Logging} from "../../services/logging/logging.service";
+import {LocationWatch} from "../../services/location";
+import {LEARNPLACE_REPOSITORY, LearnplaceRepository} from "../providers/repository/learnplace.repository";
+import {USER_REPOSITORY, UserRepository} from "../../providers/repository/repository.user";
+import {Geolocation} from "@ionic-native/geolocation";
 
 /**
  * Describes a synchronization that manages un-synchronized visit journal entries.
@@ -61,5 +65,57 @@ export class VisitJournalSynchronizationImpl implements VisitJournalSynchronizat
         this.log.debug(() => `Synchronize Journal Entry Error: ${JSON.stringify(error)}`);
       }
     }
+  }
+}
+
+/**
+ * Describes a location watch for the visit journal of a learnplace.
+ * Compares the device's location with the location of the learnplace
+ * specified by the {@link VisitJournalWatch#setLearnplace} method.
+ *
+ * If the device is close enough to the learnplace's location,
+ * the current user will be marked as someone who has visited the learnplace.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 1.0.0
+ */
+export interface VisitJournalWatch extends LocationWatch {
+
+  /**
+   * Sets the id for the learnplace to use for the visit journal.
+   *
+   * @param {number} id - the object id of the learnplace
+   */
+  setLearnplace(id: number): void;
+}
+
+/**
+ * Synchronizes the visit journal of a learnplace with ILIAS.
+ * The current user will be considered in order to know the appropriate ILIAS installation.
+ *
+ * @author nmaerchy <nm@studer-raimann.ch>
+ * @version 0.0.1
+ */
+@Injectable()
+export class SynchronizedVisitJournalWatch implements VisitJournalWatch {
+
+  private learnplaceId: number | undefined = undefined;
+
+  constructor(
+    @Inject(LEARNPLACE_API) private readonly learnplaceAPI: LearnplaceAPI,
+    @Inject(LEARNPLACE_REPOSITORY) private readonly learnplaceRepository: LearnplaceRepository,
+    @Inject(USER_REPOSITORY) private readonly userRepository: UserRepository,
+    private readonly geolocation: Geolocation
+  ) {}
+
+
+  setLearnplace(id: number): void {
+    this.learnplaceId = id;
+  }
+
+  start(): void {
+  }
+
+  stop(): void {
   }
 }
