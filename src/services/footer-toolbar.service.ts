2af0911c6b5b79760efd7385f722513430e50a1d
Nicolas Schäfli <ns@studer-raimann.ch>
2018-03-27 17:21:11 +0200

    Removed dead code from footer toolbar

--- src/services/footer-toolbar.service.ts
+++ src/services/footer-toolbar.service.ts
@@ -1,32 +1,33 @@
 import {Injectable} from "@angular/core";
 import {DownloadProgress} from "./file.service";
 import {Log} from "./log.service";
+import {Logger} from "./logging/logging.api";
+import {Logging} from "./logging/logging.service";
 
 @Injectable()
 export class FooterToolbarService {
 
-    private _offline: boolean = false;
+    private log: Logger = Logging.getLogger(FooterToolbarService.name);
 
-    constructor() {
-    }
+    offline: boolean = false;
 
-    protected _isLoading: boolean = false;
-    protected _loadingText: string = "";
-    protected _buttons: Array<{label: string, icon: string, handler: any}> = [];
-    protected _jobs: Array<{id: number, text: string}> = [];
-    protected _jobsProgress: {[id: number]: DownloadProgress} = {};
+    private _isLoading: boolean = false;
+    private _loadingText: string = "";
+    private jobs: Array<{id: number, text: string}> = [];
 
-    get isLoading() {
+    constructor() {}
+
+    get isLoading(): boolean {
         return this._isLoading;
     }
 
-    addJob(id: number, text: string) {
+    addJob(id: number, text: string): void {
         this.spliceId(id);
-        this._jobs.push({id: id, text: text});
+        this.jobs.push({id: id, text: text});
         this.updateLoading();
     }
 
-    removeJob(id: number) {
+    removeJob(id: number): void {
         this.spliceId(id);
         this.updateLoading();
     }
@@ -35,18 +36,18 @@ export class FooterToolbarService {
      * this method removes all occurences of the job with id.
      * @param id
      */
-    private spliceId(id: number) {
-        for (const key in this._jobs) {
-            if (this._jobs[key].id == id) {
-                this._jobs.splice((<any> key), 1);
+    private spliceId(id: number): void {
+        for (const key in this.jobs) {
+            if (this.jobs[key].id == id) {
+                this.jobs.splice((<any> key), 1);
             }
         }
     };
 
-    protected updateLoading() {
-        const jobs = this.countJobs();
-        Log.write(this, "number of jobs running: ", this.countJobs());
-        Log.write(this, "Currently running jobs: ", this._jobs);
+    private updateLoading(): void {
+        const jobs: number = this.countJobs();
+        this.log.debug(() => `number of jobs running: ${jobs}`);
+        this.log.debug(() => `Currently running jobs: ${this.jobs}`);
         if (jobs > 0) {
             this._isLoading = true;
             this._loadingText = this.getCurrentText();
@@ -56,30 +57,22 @@ export class FooterToolbarService {
         }
     }
 
-    protected getCurrentText(): string {
+    private getCurrentText(): string {
         // with the slice we make sure the last element is not popped from the original array.
-        if (this._jobs.slice(-1).pop()) {
-            const job: {id: number, text: string} = this._jobs.slice(-1).pop();
+        if (this.jobs.slice(-1).pop()) {
+            const job: {id: number, text: string} = this.jobs.slice(-1).pop();
             return job.text;
         }
         else
             return "";
     }
 
-    protected countJobs(): number {
-        return this._jobs.length;
+    private countJobs(): number {
+        return this.jobs.length;
     }
 
-    get loadingText() {
+    get loadingText(): string {
         return this._loadingText;
-    }
-
-    get offline(): boolean {
-        return this._offline;
-    }
-
-    set offline(value: boolean) {
-        this._offline = value;
     }
 }
 
