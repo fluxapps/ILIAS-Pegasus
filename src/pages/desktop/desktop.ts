f6a547aa1b7e61ec16c22f0c8c9c508f4cc7d83d
mschneiter <msc@studer-raimann.ch>
2019-01-28 11:08:12 +0100

    implemented opening of repo from main-tab, assuming the ref_id of all ILIAS-repos is 1

--- src/pages/desktop/desktop.ts
+++ src/pages/desktop/desktop.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
+import {Component, Inject} from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { ObjectListPage } from "../object-list/object-list";
 import { Page } from "ionic-angular/navigation/nav-util";
 import {CONFIG_PROVIDER, ILIASConfigProvider, ILIASInstallation} from "../../config/ilias-config";
+import {Builder} from "../../services/builder.base";
+import {OPEN_OBJECT_IN_ILIAS_ACTION_FACTORY, OpenObjectInILIASAction} from "../../actions/open-object-in-ilias-action";
+import {LINK_BUILDER, LinkBuilder} from "../../services/link/link-builder.service";
 
 /**
  * Generated class for the DesktopPage page.
@@ -21,15 +24,22 @@ export class DesktopPage {
   objectListPage: Page = ObjectListPage;
   readonly installations: Array<ILIASInstallation> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      @Inject(OPEN_OBJECT_IN_ILIAS_ACTION_FACTORY)
+      private readonly openInIliasActionFactory: (title: string, urlBuilder: Builder<Promise<string>>) => OpenObjectInILIASAction,
+      @Inject(LINK_BUILDER) private readonly linkBuilder: LinkBuilder
+  ) { }
 
   async switchTabs(tab: number): Promise<void> {
       this.navCtrl.parent.select(tab);
-    }
-  async openILIASRepository(): Promise<void>{
-    //Open repo in Browser inApp for iOS, external for Android
+  }
 
+  //Open repo in Browser inApp for iOS, external for Android
+  async openILIASRepository(): Promise<void>{
+      const REFID_REPOSITORY: number = 1;
+      this.openInIliasActionFactory(undefined, this.linkBuilder.default().target(REFID_REPOSITORY)).execute();
   }
 
 }
