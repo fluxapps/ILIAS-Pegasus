4cb9a22ea3f2ce0500deb6fe99b17457e77b898b
Nicolas Schäfli <ns@studer-raimann.ch>
2019-10-23 14:32:29 +0200

    Add explicit reflect-metadata package dependency for typeorm

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {AppModule} from "./app/app.module";
 import {environment} from "./environments/environment";
 import {useStandard} from "./standard";
+// Required for typeorm library
+// tslint:disable-next-line:no-import-side-effect
+import "reflect-metadata";
 
 if (environment.production) {
   enableProdMode();
