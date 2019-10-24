6842e29709edc62ace93d4201c21eef44afdc628
Nicolas Schäfli <ns@studer-raimann.ch>
2019-10-25 01:56:48 +0200

    Migrate map service to mapbox

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,8 +1,6 @@
-import {
-  CameraPosition, GoogleMap, GoogleMapOptions, GoogleMaps, GoogleMapsEvent, ILatLng, LatLng, MapType,
-  MarkerOptions, MyLocation, MyLocationOptions, VisibleRegion
-} from "@ionic-native/google-maps";
+import mapboxgl from "mapbox-gl"
 import {isUndefined} from "util";
+import {environment} from "../../environments/environment";
 
 /**
  * Describes coordinates by longitude and latitude.
@@ -46,176 +44,47 @@ export interface CameraOptions {
  *
  * @see https://ionicframework.com/docs/native/google-maps
  *
- * @author nmaerchy <nm@studer-raimann.ch>
- * @version 1.0.0
+ * @author nmaerchy <nm@studer-raimann.ch> | nschaefli <ns@studer-raimann.ch>
+ * @version 2.0.0
  */
 export interface StandardMap {
 
-  setDiv(domNode: string | HTMLElement): void
-
-  getDiv(): HTMLElement
-
-  setMapTypeId(mapTypeId: MapType): void
-
-  animateCamera(position: CameraPosition<ILatLng>): Promise<void>
+  readonly container: HTMLElement
 
   animateCameraZoomIn(): Promise<void>
 
   animateCameraZoomOut(): Promise<void>
-
-  moveCamera(position: CameraPosition<ILatLng>): Promise<void>
-
-  moveCameraZoomIn(): Promise<void>
-
-  moveCameraZoomOut(): Promise<void>
-
-  getCameraPosition(): CameraPosition<ILatLng>
-
-  getCameraTarget(): Promise<ILatLng>
-
-  getCameraZoom(): number
-
-  getCameraBearing(): number
-
-  getCameraTilt(): number
-
-  setCameraTarget(target: ILatLng): void
-
-  setCameraZoom(zoomLevel: number): void
-
-  setCameraTilt(tiltLevel: number): void
-
-  setCameraBearing(bearing: number): void
-
-  panBy(x: number, y: number): void
-
-  getVisibleRegion(): VisibleRegion
-
-  getMyLocation(options?: MyLocationOptions): Promise<MyLocation>
-
-  setClickable(isClickable: boolean): void
-
-  remove(): Promise<void>
-
-  clear(): Promise<void>
-
-  fromLatLngToPoint(latLng: ILatLng): Promise<object>
-
-  
-  fromPointToLatLng(point: Array<number>): Promise<LatLng>
-
-  setMyLocationEnabled(enabled: boolean): void
-
-  getFocusedBuilding(): Promise<object>
-
-  setIndoorEnabled(enabled: boolean): void
-
-  setTrafficEnabled(enabled: boolean): void
-
-  setCompassEnabled(enabled: boolean): void
-
-  setAllGesturesEnabled(enabled: boolean): void
-
-  setVisible(visible: boolean): void
-
-  setPadding(top: number, right: number, left: number, bottom: number): void
 }
 
 /**
- * {@link StandardMapBinding} is a abstraction from the {@link GoogleMap}.
- * Its used as a wrapper class, because some methods of the google map
+ * {@link MapboxMapBinding} is a abstraction from the {@link mapboxgl}.
+ * Its used as a wrapper class, because some methods of the mapbox map
  * should only be accessed via the {@link MapBuilder}.
  *
  * Methods are delegated to the given {@code map} instance.
  *
- * @author nmaerchy <nm@studer-raimann.ch>
+ * @author nschaefli <ns@studer-raimann.ch>
  * @version 1.0.0
+ * @since 3.0.2
  */
-class StandardMapBinding implements StandardMap {
+class MapboxMapBinding implements StandardMap {
+    constructor(
+        private readonly map: mapboxgl.Map
+    ) {}
 
-  constructor(
-    private readonly map: GoogleMap
-  ) {}
+    animateCameraZoomIn(): Promise<void> {
+        this.map.zoomIn();
+        return Promise.resolve();
+    }
 
-  setDiv(domNode: string | HTMLElement): void { this.map.setDiv(domNode) }
+    animateCameraZoomOut(): Promise<void> {
+        this.map.zoomOut();
+        return Promise.resolve();
+    }
 
-  getDiv(): HTMLElement { return this.map.getDiv() }
-
-  setMapTypeId(mapTypeId: MapType): void { this.map.setMapTypeId(mapTypeId) }
-
-  async animateCamera(position: CameraPosition<ILatLng>): Promise<void> {
-    return this.map.animateCamera(position)
-  }
-
-  animateCameraZoomIn(): Promise<void> { return this.map.animateCameraZoomIn() }
-
-  animateCameraZoomOut(): Promise<void> { return this.map.animateCameraZoomOut() }
-
-  moveCamera(position: CameraPosition<ILatLng>): Promise<void> {
-    return this.map.moveCamera(position);
-  }
-
-  moveCameraZoomIn(): Promise<void> { return this.map.moveCameraZoomIn() }
-
-  moveCameraZoomOut(): Promise<void> { return this.map.moveCameraZoomOut() }
-
-  getCameraPosition(): CameraPosition<ILatLng> { return this.map.getCameraPosition() }
-
-  async getCameraTarget(): Promise<ILatLng> { return this.map.getCameraTarget() }
-
-  getCameraZoom(): number { return this.map.getCameraZoom() }
-
-  getCameraBearing(): number { return this.map.getCameraBearing() }
-
-  getCameraTilt(): number { return this.map.getCameraTilt() }
-
-  setCameraTarget(target: ILatLng): void { this.map.setCameraTarget(target) }
-
-  setCameraZoom(zoomLevel: number): void { this.map.setCameraZoom(zoomLevel) }
-
-  setCameraTilt(tiltLevel: number): void { this.map.setCameraTilt(tiltLevel) }
-
-  setCameraBearing(bearing: number): void { this.map.setCameraBearing(bearing) }
-
-  panBy(x: number, y: number): void { this.map.panBy(x, y) }
-
-  getVisibleRegion(): VisibleRegion { return this.map.getVisibleRegion() }
-
-  async getMyLocation(options?: MyLocationOptions): Promise<MyLocation> {
-    return this.map.getMyLocation(options);
-  }
-
-  setClickable(isClickable: boolean): void { this.map.setClickable(isClickable) }
-
-  async remove(): Promise<void> { return this.map.remove() }
-
-  async clear(): Promise<void> { return this.map.clear() }
-
-  async fromLatLngToPoint(latLng: LatLng): Promise<object> {
-    return this.map.fromLatLngToPoint(latLng);
-  }
-
-  async fromPointToLatLng(point: Array<number>): Promise<LatLng> {
-    return this.map.fromPointToLatLng(point);
-  }
-
-  setMyLocationEnabled(enabled: boolean): void { this.map.setMyLocationEnabled(enabled) }
-
-  getFocusedBuilding(): Promise<object> { return this.map.getFocusedBuilding() }
-
-  setIndoorEnabled(enabled: boolean): void { this.map.setIndoorEnabled(enabled) }
-
-  setTrafficEnabled(enabled: boolean): void { this.map.setTrafficEnabled(enabled) }
-
-  setCompassEnabled(enabled: boolean): void { this.map.setCompassEnabled(enabled) }
-
-  setAllGesturesEnabled(enabled: boolean): void { this.map.setAllGesturesEnabled(enabled) }
-
-  setVisible(visible: boolean): void { this.map.setVisible(visible) }
-
-  setPadding(top: number, right: number, bottom: number, left: number): void {
-      this.map.setPadding(top, right, bottom, left);
-  }
+    get container(): HTMLElement {
+        return this.map.getContainer();
+    }
 }
 
 /**
@@ -234,24 +103,16 @@ class StandardMapBinding implements StandardMap {
 
 export class MapBuilder {
 
-  private cameraPosition?: CameraPosition<ILatLng>;
-  private markerOptions?: MarkerOptions;
+  private cameraPosition?: CameraOptions;
+  private markerOptions?: Marker;
   private binding?: string | HTMLElement;
+  private readonly DEFAULT_ZOOM: number = 13;
 
-  private readonly defaultControls: object = {
-    compass: false,
-    myLocationButton: true,
-    myLocation: true,
-    indoorPicker: false,
-    zoom: true
-  };
-
-  private readonly defaultGestures: object = {
-    rotate: true,
-    scroll: true,
-    tilt: false,
-    zoom: true
-  };
+    constructor() {
+        if (typeof mapboxgl.accessToken !== "string" || mapboxgl.accessToken.length === 0) {
+            mapboxgl.accessToken = environment.mapboxApiKey;
+        }
+    }
 
   /**
    * Uses the given {@code position} as the map camera.
@@ -262,13 +123,9 @@ export class MapBuilder {
    */
   camera(options: CameraOptions): MapBuilder {
 
-    this.cameraPosition = <CameraPosition<ILatLng>>{
-      zoom: (options.zoom == undefined)? 10 : options.zoom,
-      tilt: 0,
-      target: <ILatLng>{
-        lat: options.position.latitude,
-        lng: options.position.longitude
-      }
+    this.cameraPosition = {
+        zoom: options.zoom || this.DEFAULT_ZOOM,
+        position: options.position
     };
 
     return this;
@@ -285,18 +142,7 @@ export class MapBuilder {
    */
   marker(marker: Marker): MapBuilder {
 
-    this.markerOptions = <MarkerOptions>{
-      title: marker.title,
-      icon: {
-          "url": "/assets/icon/obj_location.svg",
-            },
-      animation: "DROP",
-      position: <ILatLng>{
-        lat: marker.position.latitude,
-        lng: marker.position.longitude
-      },
-      snippet: marker.snippet
-    };
+    this.markerOptions = marker;
 
     return this;
   }
@@ -319,7 +165,6 @@ export class MapBuilder {
    * Builds the map depending on the used options.
    *
    * This methods awaits the ready event of the created map.
-   * @see GoogleMap#one, GoogleMapsEvent#MAP_READY
    *
    * @returns {Promise<StandardMap>}
    * @throws {MapEvaluationError} if the builder is not used properly
@@ -334,20 +179,61 @@ export class MapBuilder {
       return new MapEvaluationError("Can not build map: Requires a node to bind the map")
     });
 
-    console.log(JSON.stringify(this.binding));
-    const googleMap: GoogleMap = GoogleMaps.create(this.binding, <GoogleMapOptions>{
-      camera: this.cameraPosition,
-      controls: this.defaultControls,
-      gestures: this.defaultGestures
+    const mapboxMap: mapboxgl.Map = new mapboxgl.Map({
+        container: this.binding,
+        center: [this.cameraPosition.position.longitude, this.cameraPosition.position.latitude],
+        style: "mapbox://styles/mapbox/streets-v9",
+        zoom: this.cameraPosition.zoom,
+        interactive: true
     });
 
-    await googleMap.one(GoogleMapsEvent.MAP_READY);
+      // Add geolocate control to the map.
+      mapboxMap.addControl(new mapboxgl.GeolocateControl({
+          positionOptions: {
+              enableHighAccuracy: true
+          },
+          trackUserLocation: true
+      }));
+      // Add geolocate control to the map.
+      mapboxMap.addControl(new mapboxgl.NavigationControl({
+          showCompass: true,
+          showZoom: true
+      }));
 
-    this.applyIfNotUndefined(this.markerOptions, async(options: MarkerOptions) => {
-      await googleMap.addMarker(options);
-    });
+      this.applyIfNotUndefined(this.markerOptions, (marker) => {
+          mapboxMap.once("load", () => {
+              mapboxMap.loadImage(
+                  "assets/icon/map_marker.png",
+                  (error, image) => {
+                  if (error) throw error;
+                  const imageName: string = "learnplace";
+                  mapboxMap.addImage(imageName, image);
+                  mapboxMap.addLayer({
+                      id: "points",
+                      type: "symbol",
+                      source: {
+                          type: "geojson",
+                          data: <GeoJSON.FeatureCollection<GeoJSON.Geometry>>{
+                              type: "FeatureCollection",
+                              features: [{
+                                  type: "Feature",
+                                  geometry: {
+                                      type: "Point",
+                                      coordinates: [marker.position.longitude, marker.position.latitude]
+                                  }
+                              }]
+                          }
+                      },
+                      layout: {
+                          "icon-image": imageName,
+                          "icon-size": 0.25
+                      }
+                  });
+              });
+          });
+      });
 
-    return Promise.resolve(new StandardMapBinding(googleMap));
+    return Promise.resolve(new MapboxMapBinding(mapboxMap));
   }
 
   /**
