d7519fbe491d1a9aaaa0f2a2bfaff511279b6ea7
Nicolas Schäfli <ns@studer-raimann.ch>
2019-10-24 13:56:23 +0200

    Add secure context check, required for geolocation browser api.

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,16 @@ import {useStandard} from "./standard";
 // tslint:disable-next-line:no-import-side-effect
 import "reflect-metadata";
 
+if (window.isSecureContext !== true) {
+    const errorMessage: string = "The ILIAS Pegasus App needs a secure context! " +
+        "Make sure this app is served over https://, file:// or http://localhost";
+    throw new Error(errorMessage);
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
 useStandard();
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
