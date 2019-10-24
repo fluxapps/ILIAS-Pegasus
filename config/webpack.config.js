21a3178807902466b95fd703c5f8a8cef94dc6f7
Nicolas Schäfli <ns@studer-raimann.ch>
2019-10-25 01:58:10 +0200

    Revert webpack config changes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -155,30 +155,4 @@ const prodConfig = {
 module.exports = {
   dev: devConfig,
   prod: prodConfig,
-
-
-  entry: [
-    "./index.js"
-  ],
-
-  output: {
-    path: __dirname,
-    filename: "bundle.js",
-
-    // export the bundle as a CommonJS module
-    // http://webpack.github.io/docs/configuration.html#output-librarytarget
-    libraryTarget: 'commonjs'
-  },
-
-  // do not touch native Node modules (e.g. fs)
-  // http://webpack.github.io/docs/configuration.html#target
-  target: 'node',
-
-  externals: {
-    // define newrelic as an external library
-    // http://webpack.github.io/docs/configuration.html#externals
-    newrelic: true
-  }
-
-
 };
