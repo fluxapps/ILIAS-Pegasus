030ccc95825cc76882b102fdc8c7e1c64aaccacb
Nicolas Schäfli <ns@studer-raimann.ch>
2019-10-24 18:44:03 +0200

    Fix learnplace map component

--- src/app/learnplace/pages/map/map.module.ts
+++ src/app/learnplace/pages/map/map.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        TranslateModule,
+        TranslateModule.forChild(),
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
