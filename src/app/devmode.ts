7fb6b9a8281a210ee3b31c7d7b298c73103a917c
Nicolas Schäfli <ns@studer-raimann.ch>
2019-10-28 17:23:05 +0100

    Fix devmode detection

--- src/app/devmode.ts
+++ src/app/devmode.ts
@@ -1,26 +1,13 @@
-import {isDefined} from "./util/util.function";
-import {isFunction} from "util";
-
-//import {isDevMode as isAngularDevMode} from "@angular/core";
+import {environment} from "../environments/environment";
 
 /**
  * Returns true if the dev mode is enabled, otherwise returns false.
  *
  * The app is considered to run in dev mode if one of the following conditions are met:
- * - If the ionic dev server is running
- * - If the angular dev mode is enabled.
+ * - If environment.production is set to false.
  *
  * @returns {boolean} true if the pegasus dev mode is enabled, otherwise false.
  */
 export function isDevMode(): boolean {
-    const monitor: IonicDevServer = <IonicDevServer>window["IonicDevServer"];
-
-    return    isDefined(monitor) &&
-        isFunction(monitor.handleError);
-    // ||
-    // isAngularDevMode();
-}
-
-interface IonicDevServer {
-    handleError(): void
+    return !environment.production;
 }
