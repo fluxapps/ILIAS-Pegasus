a465bac75c4c29c03b346ee69aabcf1c92c7ca9b
Nicolas Schäfli <ns@studer-raimann.ch>
2019-11-06 14:34:24 +0100

    Fix filesystem type imports

--- src/app/services/filesystem/filesystem.module.ts
+++ src/app/services/filesystem/filesystem.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from "@angular/core";
-import {FileOpener} from "@ionic-native/file-opener";
-import {File} from "@ionic-native/file";
+import {FileOpener} from "@ionic-native/file-opener/ngx";
+import {File} from "@ionic-native/file/ngx";
 import {CORDOVA_FILESYSTEM_FACTORY} from "./filesystem.cordova";
 import {FILESYSTEM_TOKEN} from "./filesystem.service";
 import { Platform } from "@ionic/angular";
 
 @NgModule({
     providers: [
+        FileOpener,
         {
             provide: FILESYSTEM_TOKEN,
             useFactory: CORDOVA_FILESYSTEM_FACTORY,
