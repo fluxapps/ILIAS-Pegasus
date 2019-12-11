a27ba820e702557a4fef5dc73db91ec73ed8d467
Nicolas Schäfli <ns@studer-raimann.ch>
2019-12-11 20:47:31 +0100

    Read object descriptor to create spy objects

--- src/test.util.spec.ts
+++ src/test.util.spec.ts
@@ -1,9 +1,32 @@
+function isFunction(descriptor: PropertyDescriptor): boolean {
+    return typeof descriptor.value === "function";
+}
+
+function isProperty(descriptor: PropertyDescriptor): boolean {
+    return (
+        typeof descriptor.get !== "undefined" ||
+        typeof descriptor.set !== "undefined" ||
+        (typeof descriptor.value !== "symbol" && typeof descriptor.value !== "function")
+    );
+}
+
 export function createSpyObject<T>(ctor: new(...args: Array<unknown>) => T): jasmine.SpyObj<T> {
     const proto: unknown = ctor.prototype;
-    const propertyNames: ReadonlyArray<string> = Object.getOwnPropertyNames(proto);
+    if (typeof proto === "undefined") {
+        throw new Error("Can not mock object with undefined prototype!");
+    }
+    const propertyDescriptors: Array<[string, PropertyDescriptor]> = Object.entries(Object.getOwnPropertyDescriptors(proto));
+    const validPropertyNames: ReadonlyArray<string> = propertyDescriptors
+        .filter((it) => isProperty(it[1]))
+        .map((it) => it[0]);
+
+    const validFunctionNames: ReadonlyArray<string> = propertyDescriptors
+        .filter((it) => isFunction(it[1]))
+        .map((it) => it[0]);
+
     return jasmine.createSpyObj(
         ctor.name,
-        propertyNames.filter((it) => typeof proto[it] === "function"),
-        propertyNames.filter((it) => typeof proto[it] !== "function")
+        validFunctionNames,
+        validPropertyNames
     );
 }
