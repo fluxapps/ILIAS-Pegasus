48c36aa1fb6486b16d343d1b00ac8f3aeeae436c
mschneiter <msc@studer-raimann.ch>
2020-04-03 09:04:25 +0200

    correction of scorm player init filie for previous commit

--- branding/common/scormplayer/init.js
+++ branding/common/scormplayer/init.js
@@ -1,5 +1,5 @@
 PlayerConfiguration.Debug = false;
-PlayerConfiguration.StorageSupport = false;
+PlayerConfiguration.StorageSupport = true;
 
 PlayerConfiguration.TreeMinusIcon = "assets/scorm/player/scormpool/Img/minus.gif";
 PlayerConfiguration.TreePlusIcon = "assets/scorm/player/scormpool/Img/plus.gif";
@@ -13,8 +13,5 @@ PlayerConfiguration.BtnExitAllLabel = "Exit All";
 PlayerConfiguration.BtnAbandonLabel = "Abandon";
 PlayerConfiguration.BtnAbandonAllLabel = "Abandon All";
 PlayerConfiguration.BtnSuspendAllLabel = "Suspend All";
-//_app_file_/storage/emulated/0/Android/data/ch.studerraimann.ilias.offline/ilias-app/user1/
-//learning_modules/lm_1651/
-//Run.ManifestByURL("assets/scorm/module/imsmanifest.xml", true);
 
-Run.ManifestByURL("_app_file_/storage/emulated/0/Android/data/ch.studerraimann.ilias.offline/ilias-app/user1/learning_modules/module/imsmanifest.xml", true);
+//Run.ManifestByURL("[PATH_MODULE]/imsmanifest.xml", true);
