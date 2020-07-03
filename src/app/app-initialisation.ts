f66e666345a3fa5e8798f93b98d0c2a93cf88e69
Nicolas Schäfli <ns@studer-raimann.ch>
2020-07-03 16:14:24 +0200

    Fix language detection while no user session is present

--- src/app/app-initialisation.ts
+++ src/app/app-initialisation.ts
@@ -1,19 +1,20 @@
 import { APP_INITIALIZER, FactoryProvider } from "@angular/core";
-import { Router } from "@angular/router";
 import { Network } from "@ionic-native/network/ngx";
 import { SQLite } from "@ionic-native/sqlite/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
-import { Config, NavController, Platform, ToastController } from "@ionic/angular";
+import { Platform } from "@ionic/angular";
 import { TranslateService } from "@ngx-translate/core";
 import { PEGASUS_CONNECTION_NAME } from "./config/typeORM-config";
 import { Settings } from "./models/settings";
 import { User } from "./models/user";
-import { ObjectListPage } from "./pages/object-list/object-list";
 import { AuthenticationProvider } from "./providers/authentication.provider";
 import { SQLiteDatabaseService } from "./services/database.service";
 import { Database } from "./services/database/database";
 import { DB_MIGRATION, DBMigration } from "./services/migration/migration.api";
 
+
+const FALLBACK_LANGUAGE: string = "de";
+const SUPPORTED_LANGUAGES: Set<string> = new Set(["de", "en", "it"]);
 
 export const INIT_APP: FactoryProvider = {
     provide: APP_INITIALIZER,
@@ -24,11 +25,7 @@ export const INIT_APP: FactoryProvider = {
         database: Database,
         migration: DBMigration,
         translate: TranslateService,
-        statusBar: StatusBar,
-        navCtrl: NavController,
-        toastCtrl: ToastController,
-        router: Router,
-        config: Config
+        statusBar: StatusBar
     ): (() => Promise<void>) => async(): Promise<void> => {
         // cordova ready
         await platform.ready();
@@ -48,16 +45,26 @@ export const INIT_APP: FactoryProvider = {
             translate.use(setting.language);
         } else {
             // get the language of the navigator an check if it is supported. default is de
-            let lng: string = "de";
-            const navLng: string = navigator.language.split("-")[0];
-            ["de", "en", "it"].forEach(s => {
-                if (navLng.match(`/${s}/i`)) {
-                    lng = s;
+            const language: string = !!navigator.language ? navigator.language : FALLBACK_LANGUAGE;
+            const languages: ReadonlyArray<string> = !!navigator.languages && navigator.languages.length > 0 ?
+                navigator.languages : [FALLBACK_LANGUAGE];
+
+            const browserLanguages: Set<string> = Intl
+                // @ts-ignore (TS 3.5 does not have a type dev for this intl function)
+                .getCanonicalLocales([language, ...languages])
+                .map((it) => it.split("-")[0])
+                .reduceRight((col, child) => col.add(child), new Set()); // Sets in js are ordered
+
+            let lng: string = FALLBACK_LANGUAGE;
+            for (const langCode of browserLanguages) {
+                if (SUPPORTED_LANGUAGES.has(langCode)) {
+                    lng = langCode;
+                    break;
                 }
-            });
+            }
             translate.use(lng);
         }
-        translate.setDefaultLang("de");
+        translate.setDefaultLang(FALLBACK_LANGUAGE);
 
         statusBar.styleLightContent();
     },
